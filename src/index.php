34e7bcb0e2a084be1c38bd19baa48f409347fa8e
Matthew Tan <snowfall91@live.com>
2016-02-04 16:02:45 +0800

    initial commit of index

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+
+        <form action="results.php" method="post">
+        <input type="text" query="itemName"><br>
+        <input type="submit" value="Borrow!">
+        </form>
+
+    </body>
+</html>
