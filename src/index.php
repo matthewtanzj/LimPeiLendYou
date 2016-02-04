565b6478db49fefe2ec407eef98d1f3b61ece71c
Matthew Tan <snowfall91@live.com>
2016-02-04 20:54:02 +0800

    add metadata for index

--- src/index.php
+++ src/index.php
@@ -1,10 +1,25 @@
-<html>
-    <body>
+<!DOCTYPE html>
+<html lang="en">
 
+<html>
+
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="author" content="">
+        
+		<title>WhoBorrow</title>
+	</head>
+	
+    <body>
         <form action="results.php" method="post">
         <input type="text" query="itemName"><br>
         <input type="submit" value="Borrow!">
         </form>
-
     </body>
+	
 </html>
