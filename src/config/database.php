12787fc7b68fa5ce2f2b6f8639a41acdeb90fd35
jackyhsuch <jackyhsu0102@gmail.com>
2016-02-22 03:32:53 +0800

    added config file for database

---
+++ src/config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+$db = array(
+	'hostname' => 'localhost',
+	'username' => 'homestead',
+	'password' => 'secret',
+	'database' => 'whoborrow',
+	'port' => '5432',
+);
--- src/include/db_connect.php
+++ src/include/db_connect.php
@@ -1,4 +1,10 @@
 <?php
-$dbconn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=postgres") 
-						or die('Could not connect: ' . pg_last_error());
-?>
+include("config/database.php");
+
+$dbconn = pg_connect(
+			"host=" . $db['hostname'] . 
+			" port=" . $db['port'] . 
+			" dbname=" . $db['database'] . 
+			" user=" . $db['username'] . 
+			" password=" . $db['password']) 
+			or die('Could not connect: ' . pg_last_error());
