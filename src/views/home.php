9f4bc74c6b123bab4fb1f9fd60f38f697a515285
Matthew Tan <snowfall91@live.com>
2016-03-01 13:37:04 +0800

    localized jquery js file

--- src/views/home.php
+++ src/views/home.php
@@ -13,7 +13,10 @@
         <!-- Bootstrap -->
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="js/jquery-1.12.1.min.js"></script>
+		<!--
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		-->
         <!-- Include all compiled plugins (below), or include individual files as needed -->
         <script src="js/bootstrap.min.js"></script>
         <!-- Custom styles for this template -->
