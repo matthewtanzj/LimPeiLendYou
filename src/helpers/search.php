c3ea6f9ab7c9cd1b154ce0afa04b4d921a8cb72a
jackyhsuch <jackyhsu0102@gmail.com>
2016-03-29 22:19:28 +0800

    remove search in helpers

--- src/helpers/search.php
+++
@@ -1,7 +0,0 @@
-<div class="input-group">
-    <input type="text" class="form-control" placeholder="Search for...">
-    <span class="input-group-btn">
-        <button class="btn btn-default" type="button">Search!</button>
-    </span>
-</div><!-- /input-group -->
-
