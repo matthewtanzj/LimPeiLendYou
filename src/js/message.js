a84f1c6c0b152ff9710c6a4da201e4d1b8fa72d1
Victor Hazali <victorhazali@gmail.com>
2016-04-04 02:11:37 +0800

    Added script to add new messages to message box

---
+++ src/js/message.js
@@ -0,0 +1,25 @@
+function sendMessage(user) {
+	var msgBox = document.getElementById("message-content");
+	var content = msgBox.value;
+	msgBox.value='';
+
+	if (content == '') {
+		return;
+	} 
+
+	window.alert(user);
+
+	var preContentStr = "<div class=\"media msg \"><a class=\"pull-left\" href=\"#\"><img class=\"media-object\" data-src=\"holder.js/64x64\" alt=\"64x64\" style=\"width: 32px; height: 32px;\" src=\"\"></a><div class=\"media-body\"><small class=\"pull-right time\"><i class=\"fa fa-clock-o\"></i>timestamp</small><h5 class=\"media-heading\"><?php echo $_GET['owner']?></h5><small class=\"col-lg-10\">";
+	var postContentStr = "</small></div></div>"
+	var toAppend = preContentStr + content + postContentStr;
+	$("#msg-box").append(toAppend);
+
+
+	updateScroll();
+	msgBox.focus();
+}
+
+function updateScroll(){
+    var element = document.getElementById("msg-box");
+    element.scrollTop = element.scrollHeight;
+}
--- src/views/message.php
+++ src/views/message.php
@@ -25,10 +25,15 @@
         <!-- Include all compiled plugins (below), or include individual files as needed -->
         <script src="js/bootstrap.min.js"></script>
         <!-- Message scripts -->
+        <script src="js/message.js"></script>
         <!-- <script src="js/message.js"></script> -->
         <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
         <!-- Custom styles for this template -->
         <link href="css/messageStyle.css" rel="stylesheet">
+        <!-- date picker -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/css/bootstrap-datepicker3.min.css" >
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/css/bootstrap-datepicker3.standalone.min.css" >
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/js/bootstrap-datepicker.min.js"></script>
     </head>
     
     <body>
@@ -36,29 +41,26 @@
         <!-- Fixed navbar -->
         <?php include 'views/navbar.php' ?>
         
+        <!-- Item toolbar -->
+
         <div class="container message-container">
             <div class="row">
-
-                <!-- Item toolbar -->
-
-                <!-- Receiver Profile -->
-
                 <!-- Entire Chat box -->
                 <div class="message-wrap col-lg-10">
 
                     <!-- Sent and received messages box -->
-                    <div class="msg-wrap">
+                    <div id="msg-box" class="msg-wrap">
 
                         <!-- Message from receiver -->
                         <div class="media msg ">
-                            <!-- Sender Profile Icon -->
+                            <!-- Receiver Profile Icon -->
                             <a class="pull-left" href="#">
                                 <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="">
                             </a>
                             <!-- Message Contents -->
                             <div class="media-body">
                                 <small class="pull-right time"><i class="fa fa-clock-o"></i>timestamp</small>
-                                <h5 class="media-heading">Sender name</h5>
+                                <h5 class="media-heading"><?php echo $_GET['owner']?></h5>
                                 <small class="col-lg-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consequat tempor ex, quis pulvinar ante consectetur in. Sed efficitur, eros id mollis posuere, nibh magna maximus tellus, sit amet dapibus enim est quis tellus. Curabitur ut leo ultrices, egestas eros id, mattis mauris. Vivamus viverra ut erat ut porta. Nulla vitae pellentesque odio, id feugiat odio. Proin molestie aliquet ex nec maximus. In ornare tristique sem malesuada auctor. Nullam porttitor, tortor vel dignissim finibus, felis elit finibus purus, semper tincidunt diam nibh id lacus.</small>
                             </div>
                         </div>
@@ -70,7 +72,7 @@
                             </a>
                             <div class="media-body">
                                 <small class="pull-left time"><i class="fa fa-clock-o"></i>timestamp</small>
-                                <h5 class="media-heading pull-right">Sender name</h5>
+                                <h5 class="media-heading pull-right"><?php echo $_SESSION['username']?></h5>
                                 <br>
                                 <small class="col-lg-10 pull-right sender-message">Maecenas sed laoreet augue. Sed mattis elementum dui, quis consectetur ipsum. Aenean quis augue nec turpis rutrum lobortis quis vitae ante. Quisque at interdum nulla, hendrerit finibus purus. Quisque sit amet scelerisque sapien, nec placerat quam. Aenean mollis ante quis lectus auctor molestie. Aenean vitae dui a massa ullamcorper condimentum a et nisi. Nam ante elit, consequat ut felis ac, congue aliquet nisi.</small>
                             </div>
@@ -111,8 +113,35 @@
                             <!-- Message Contents -->
                             <div class="media-body">
                                 <small class="pull-right time"><i class="fa fa-clock-o"></i>timestamp</small>
-                                <h5 class="media-heading">Sender name</h5>
+                                <h5 class="media-heading">Receiver name</h5>
                                 <small class="col-lg-10">Phasellus quis erat porta tellus maximus tristique. Cras ornare felis eget iaculis tincidunt. Nam quam nunc, dictum in faucibus id, bibendum ut sapien. Donec at malesuada mauris. Vestibulum consequat metus justo, quis ultricies lectus consequat vel. Nulla consequat lectus ac augue interdum bibendum. Etiam egestas tortor eu massa sodales pharetra. Curabitur varius pulvinar arcu, quis efficitur ante vehicula a. Aenean nec blandit nulla. Phasellus congue tristique velit. Fusce consectetur pellentesque ligula in tincidunt. Fusce ultricies dui eros, at auctor lorem cursus a. Aliquam erat volutpat. Morbi quis porttitor lectus.</small>
+                            </div>
+                        </div>
+
+                        <!-- Message from receiver -->
+                        <div class="media msg ">
+                            <!-- Sender Profile Icon -->
+                            <a class="pull-left" href="#">
+                                <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="">
+                            </a>
+                            <!-- Message Contents -->
+                            <div class="media-body">
+                                <small class="pull-right time"><i class="fa fa-clock-o"></i>timestamp</small>
+                                <h5 class="media-heading">Receiver name</h5>
+                                <small class="col-lg-10">Phasellus quis erat porta tellus maximus tristique. Cras ornare felis eget iaculis tincidunt. Nam quam nunc, dictum in faucibus id, bibendum ut sapien. Donec at malesuada mauris. Vestibulum consequat metus justo, quis ultricies lectus consequat vel. Nulla consequat lectus ac augue interdum bibendum. Etiam egestas tortor eu massa sodales pharetra. Curabitur varius pulvinar arcu, quis efficitur ante vehicula a. Aenean nec blandit nulla. Phasellus congue tristique velit. Fusce consectetur pellentesque ligula in tincidunt. Fusce ultricies dui eros, at auctor lorem cursus a. Aliquam erat volutpat. Morbi quis porttitor lectus.</small>
+                            </div>
+                        </div>
+
+                        <!-- Message from sender -->
+                        <div class="media msg ">
+                            <a class="pull-right" href="#">
+                                <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="">
+                            </a>
+                            <div class="media-body">
+                                <small class="pull-left time"><i class="fa fa-clock-o"></i>timestamp</small>
+                                <h5 class="media-heading pull-right">Sender name</h5>
+                                <br>
+                                <small class="col-lg-10 pull-right sender-message">Etiam auctor fringilla dui sit amet rutrum. Sed id lorem eu magna fermentum consectetur in vitae quam. Etiam non risus dolor. Fusce dui purus, rhoncus vel cursus vel, eleifend sit amet eros. Suspendisse malesuada odio nec ultrices convallis. Cras rutrum massa ipsum, eget egestas lorem tempus ut. Curabitur porttitor, purus vel mollis laoreet, eros ante varius enim, et pharetra elit tortor sed risus. Sed nunc ante, luctus non sem at, facilisis interdum ipsum. Curabitur at efficitur tortor. Vivamus non tortor quis eros eleifend condimentum sed ac est. Quisque auctor magna a velit accumsan, sit amet egestas dolor vulputate. Vestibulum sit amet leo ut enim volutpat ultricies eget egestas nulla. Donec bibendum arcu augue, scelerisque aliquam ex gravida vitae.</small>
                             </div>
                         </div>
 
@@ -125,14 +154,14 @@
                     <!-- Send message box -->
                     <div class="send-wrap ">
 
-                        <textarea class="form-control send-message" rows="3" placeholder="Write a reply..."></textarea>
+                        <textarea id="message-content" class="form-control send-message" rows="3" placeholder="Write a reply..."></textarea>
 
                     </div>
 
                     <!-- Additional buttons to include attachments -->
                     <div class="btn-panel">
                         <a href="" class=" col-lg-3 btn   send-message-btn " role="button"><i class="fa fa-cloud-upload"></i> Add Files</a>
-                        <a href="" class=" col-lg-4 text-right btn   send-message-btn pull-right" role="button"><i class="fa fa-plus"></i> Send Message</a>
+                        <a onclick="sendMessage('<?php echo $_SESSION['username']?>')" class=" col-lg-4 text-right btn send-message-btn pull-right"><i class="fa fa-plus"></i> Send Message</a>
                     </div>
 
                 </div>
