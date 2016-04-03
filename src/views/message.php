74ec0552b964c183df7630fb148d83c37a2a971b
Victor Hazali <victorhazali@gmail.com>
2016-04-03 17:39:23 +0800

    Added code for messaging function

--- src/views/message.php
+++ src/views/message.php
@@ -1,3 +1,8 @@
+<!-- 
+  Relies heavily on the chat widget "Chat" by hardiksondagar on bootsnip.com
+  The original code snippet can be found here: http://bootsnipp.com/snippets/featured/chat
+-->
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -19,15 +24,178 @@
         -->
         <!-- Include all compiled plugins (below), or include individual files as needed -->
         <script src="js/bootstrap.min.js"></script>
+        <!-- Message scripts -->
+        <!-- <script src="js/message.js"></script> -->
+        <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
         <!-- Custom styles for this template -->
-        <link href="css/style.css" rel="stylesheet">
+        <link href="css/messageStyle.css" rel="stylesheet">
     </head>
     
     <body>
 
         <!-- Fixed navbar -->
         <?php include 'views/navbar.php' ?>
-
         
+        <div class="container">
+            <div class="row">
+
+                <!-- Item toolbar -->
+
+                <!-- Receiver Profile -->
+
+                <!-- Entire Chat box -->
+                <div class="message-wrap col-lg-10">
+
+                    <!-- Sent and received messages box -->
+                    <div class="msg-wrap">
+
+                        <!-- One message -->
+                        <div class="media msg ">
+                            <!-- Sender Profile Icon -->
+                            <a class="pull-left" href="#">
+                                <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="">
+                            </a>
+                            <!-- Message Contents -->
+                            <div class="media-body">
+                                <small class="pull-right time"><i class="fa fa-clock-o"></i>timestamp</small>
+                                <h5 class="media-heading">Sender name</h5>
+                                <small class="col-lg-10">Message content</small>
+                            </div>
+                        </div>
+
+                        <div class="media msg ">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="">
+                            </a>
+                            <div class="media-body">
+                                <small class="pull-right time"><i class="fa fa-clock-o"></i>timestamp</small>
+                                <h5 class="media-heading">Sender name</h5>
+                                <small class="col-lg-10">Message content</small>
+                            </div>
+                        </div>
+                        <div class="media msg ">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="">
+                            </a>
+                            <div class="media-body">
+                                <small class="pull-right time"><i class="fa fa-clock-o"></i>timestamp</small>
+                                <h5 class="media-heading">Sender name</h5>
+                                <small class="col-lg-10">Message content</small>
+                            </div>
+                        </div>
+                        <div class="media msg ">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="">
+                            </a>
+                            <div class="media-body">
+                                <small class="pull-right time"><i class="fa fa-clock-o"></i>timestamp</small>
+                                <h5 class="media-heading">Sender name</h5>
+                                <small class="col-lg-10">Message content</small>
+                            </div>
+                        </div>
+                        <div class="media msg ">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="">
+                            </a>
+                            <div class="media-body">
+                                <small class="pull-right time"><i class="fa fa-clock-o"></i>timestamp</small>
+                                <h5 class="media-heading">Sender name</h5>
+                                <small class="col-lg-10">Message content</small>
+                            </div>
+                        </div>
+                        <div class="media msg ">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="">
+                            </a>
+                            <div class="media-body">
+                                <small class="pull-right time"><i class="fa fa-clock-o"></i>timestamp</small>
+                                <h5 class="media-heading">Sender name</h5>
+                                <small class="col-lg-10">Message content</small>
+                            </div>
+                        </div>
+                        <div class="media msg ">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="">
+                            </a>
+                            <div class="media-body">
+                                <small class="pull-right time"><i class="fa fa-clock-o"></i>timestamp</small>
+                                <h5 class="media-heading">Sender name</h5>
+                                <small class="col-lg-10">Message content</small>
+                            </div>
+                        </div>
+                        <div class="media msg ">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="">
+                            </a>
+                            <div class="media-body">
+                                <small class="pull-right time"><i class="fa fa-clock-o"></i>timestamp</small>
+                                <h5 class="media-heading">Sender name</h5>
+                                <small class="col-lg-10">Message content</small>
+                            </div>
+                        </div>
+                        <div class="media msg ">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="">
+                            </a>
+                            <div class="media-body">
+                                <small class="pull-right time"><i class="fa fa-clock-o"></i>timestamp</small>
+                                <h5 class="media-heading">Sender name</h5>
+                                <small class="col-lg-10">Message content</small>
+                            </div>
+                        </div>
+                        <div class="media msg ">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="">
+                            </a>
+                            <div class="media-body">
+                                <small class="pull-right time"><i class="fa fa-clock-o"></i>timestamp</small>
+                                <h5 class="media-heading">Sender name</h5>
+                                <small class="col-lg-10">Message content</small>
+                            </div>
+                        </div>
+                        <div class="media msg ">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="">
+                            </a>
+                            <div class="media-body">
+                                <small class="pull-right time"><i class="fa fa-clock-o"></i>timestamp</small>
+                                <h5 class="media-heading">Sender name</h5>
+                                <small class="col-lg-10">Message content</small>
+                            </div>
+                        </div>
+                        <div class="media msg ">
+                            <a class="pull-left" href="#">
+                                <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 32px; height: 32px;" src="">
+                            </a>
+                            <div class="media-body">
+                                <small class="pull-right time"><i class="fa fa-clock-o"></i>timestamp</small>
+                                <h5 class="media-heading">Sender name</h5>
+                                <small class="col-lg-10">Message content</small>
+                            </div>
+                        </div>
+
+                        <!-- date separator -->
+                        <div class="alert alert-info msg-date">
+                            <strong>Today</strong>
+                        </div>
+                    </div>
+
+                    <!-- Send message box -->
+                    <div class="send-wrap ">
+
+                        <textarea class="form-control send-message" rows="3" placeholder="Write a reply..."></textarea>
+
+                    </div>
+
+                    <!-- Additional buttons to include attachments -->
+                    <div class="btn-panel">
+                        <a href="" class=" col-lg-3 btn   send-message-btn " role="button"><i class="fa fa-cloud-upload"></i> Add Files</a>
+                        <a href="" class=" col-lg-4 text-right btn   send-message-btn pull-right" role="button"><i class="fa fa-plus"></i> Send Message</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
     </body>
 </html>
